e294947079c941e87b997a459365c8894c1c4f1d
Heliberto Arias <helibertoarias@gmail.com>
2022-03-03 22:12:11 -0500

    comment some usings

--- src/14-SOLID/TradeProcessorConsoleApp/Program.cs
+++ src/14-SOLID/TradeProcessorConsoleApp/Program.cs
@@ -1,11 +1,12 @@
-﻿using TradeProcessorConsoleApp.Final;
-using TradeProcessorConsoleApp.Version01;
-using TradeProcessorConsoleApp.Version02;
-using TradeProcessorConsoleApp.Version03;
+﻿//using TradeProcessorConsoleApp.Final;
+//using TradeProcessorConsoleApp.Version01;
+//using TradeProcessorConsoleApp.Version02;
+//using TradeProcessorConsoleApp.Version03;
 
 
 //Manager_V1.Run();
 //Manager_V2.Run();
 //Manager_V3.Run();
-Manager_Final.Run();
+//Manager_Final.Run();
 
+var A = String.Empty;
