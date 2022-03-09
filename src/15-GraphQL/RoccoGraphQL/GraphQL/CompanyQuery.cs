3fa5d5930ed8180f2262ab4aae31420ac351db69
Heliberto Arias <helibertoarias@gmail.com>
2022-03-08 23:49:30 -0500

    Adding query for one company

--- src/15-GraphQL/RoccoGraphQL/GraphQL/CompanyQuery.cs
+++ src/15-GraphQL/RoccoGraphQL/GraphQL/CompanyQuery.cs
@@ -2,6 +2,7 @@
 // Copyright (c) 2022, Heliberto Arias
 // </copyright>
 
+using GraphQL;
 using GraphQL.Types;
 using RoccoGraphQL.Application.Contracts.Persistence;
 using RoccoGraphQL.GraphQL.Types;
@@ -15,6 +16,16 @@ public class CompanyQuery : ObjectGraphType
         Field<ListGraphType<CompanyType>>(
             name: "companies",
             resolve: context => companyRepository.FindAll(false)
-            );
+        );
+
+        Field<CompanyType>(
+            name: "company",
+            arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>>
+            { Name = "id" }),
+            resolve: context =>
+           {
+               var id = context.GetArgument<Guid>("id");
+               return companyRepository.FindOneByCondition(x => x.Id == id, false);
+           });
     }
 }
